fdf4dd3ede7b83176bfe64c7325585b85049718a
Lucho <galvariniluciano@gmail.com>
2022-06-28 01:02:01 -0300

    upgrade: pve console

--- src/components/sideBar/PvE/PvEBattle.jsx
+++ src/components/sideBar/PvE/PvEBattle.jsx
@@ -3,9 +3,8 @@ import axios from "axios";
 import Cookies from "universal-cookie";
 import { useLocation } from "react-router-dom";
 import UserCard from "../../userProfile/UserCard";
-import { Table } from "react-bootstrap";
 import NpcCard from "./NpcCard";
-import { MDBTable, MDBTableBody, MDBTableHead } from "mdbreact";
+import { Table } from "react-bootstrap";
 
 const PvEBattle = () => {
   const cookies = new Cookies();
@@ -38,35 +37,10 @@ const PvEBattle = () => {
     setBattleData(location.state.battleData);
   }, []);
 
-  const data = {
-    columns: [
-      {
-        label: "Round",
-        field: "id",
-        sort: "asc",
-      },
-      {
-        label: "User life",
-        field: "userlife",
-        sort: "asc",
-      },
-      {
-        label: "User Damage",
-        field: "userdamage",
-        sort: "asc",
-      },
-      {
-        label: "Npc life",
-        field: "npclife",
-        sort: "asc",
-      },
-      {
-        label: "Npc Damage",
-        field: "npcdamage",
-        sort: "asc",
-      },
-    ],
-  };
+  const npcName = location.state.battleData.nameData.replace(
+    /(^\w{1})/g,
+    (letter) => letter.toUpperCase()
+  );
 
   return (
     <div className="pvebattle">
@@ -89,12 +63,25 @@ const PvEBattle = () => {
       </div>
 
       <div className="rounds--console">
-        {/* <MDBTable scrollY small bordered>
-          <MDBTableHead columns={data.columns} />
-          <MDBTableBody rows={battleData} />
-        </MDBTable> */}
+        <div className="history-box">
+          {battleData?.map((rounds) => (
+            <ul key={rounds.round} className="round">
+              <h6>Round: {rounds.round}</h6>
+              <div>
+                <li>
+                  {profile.username} has attacked {npcName} for {rounds.userDmg}{" "}
+                  damage. ({npcName} has {rounds.NpcLife} life)
+                </li>
+                <li>
+                  {npcName} has attacked {profile.username} for {rounds.npcDmg}
+                  damage. ({profile.username} has {rounds.userLife} life)
+                </li>
+              </div>
+            </ul>
+          ))}
+        </div>
 
-        <div className="history--box">
+        {/* <div className="history--box--2">
           <Table striped bordered hover>
             <thead>
               <tr>
@@ -117,30 +104,7 @@ const PvEBattle = () => {
               ))}
             </tbody>
           </Table>
-        </div>
-
-        {/* <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>Round</th>
-              <th>User life</th>
-              <th>User dmg</th>
-              <th>Npc life</th>
-              <th>Npc dmg</th>
-            </tr>
-          </thead>
-          <tbody className="rounds">
-            {battleData?.map((rounds) => (
-              <tr key={rounds.round}>
-                <td>{rounds.round}</td>
-                <td>{rounds.userLife}</td>
-                <td>{rounds.userDmg}</td>
-                <td>{rounds.NpcLife}</td>
-                <td>{rounds.NpcDmg}</td>
-              </tr>
-            ))}
-          </tbody>
-        </Table> */}
+        </div> */}
         <a href="/profile" className="button--links links m-3 pe-5 ps-5">
           Profile
         </a>
