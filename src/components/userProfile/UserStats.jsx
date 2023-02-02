f1c51b04bf2ae1ab3434f012095f063efc4e1986
Luciano <galvariniluciano@gmail.com>
2023-02-02 00:15:02 -0300

    update

--- src/components/userProfile/UserStats.jsx
+++ src/components/userProfile/UserStats.jsx
@@ -31,11 +31,6 @@ const UserStats = ({
     { id: 5, skill: "luck" },
   ];
 
-  const [clickAddSkill, setClickAddSkill] = React.useState({
-    stat: "",
-    amount: 0,
-  });
-
   const [stats, setStats] = useState({
     freeSkillPoints,
     strength,
@@ -63,11 +58,10 @@ const UserStats = ({
     stats.criticalChance = userStats.newCriticalChance;
   }
 
-  async function handleClickAddSkill(e) {
-    e.preventDefault();
+  async function handleClickAddSkill(skillName) {
     const response = await post(
-      "/api/v1/users/add-skill-points",
-      { skillPointName: clickAddSkill.stat, amount: clickAddSkill.amount },
+      "/api/v1/users/add-skill-points/" + skillName,
+      {},
       headers
     );
 
@@ -87,28 +81,6 @@ const UserStats = ({
       });
     }
   }
-  /*------------------------------- COMUNICACION ENTRE COMPONENTE ------------------------------- */
-  // window.addEventListener("message", (event) => {});
-  /*------------------------------- COMUNICACION ENTRE COMPONENTE ------------------------------- */
-
-  function handleChangeAmount(e) {
-    if (e.target.value < 0) {
-      e.target.value = e.target.value * -1;
-    } else if (e.target.value > freeSkillPoints) {
-      e.target.value = freeSkillPoints;
-    }
-
-    const newValues = {
-      stat: e.target.id,
-      amount: e.target.value,
-    };
-
-    setClickAddSkill(newValues);
-  }
-
-  const handleKeyPress = (event) => {
-    if (event.keyCode === 13) handleClickAddSkill();
-  };
 
   return (
     <div>
@@ -128,21 +100,15 @@ const UserStats = ({
           <div className="userstats--add">
             {data.map((zone) => (
               <div key={zone.id} className="userstats--add--form">
-                <input
-                  type="text"
-                  className="form-control"
-                  onChange={handleChangeAmount}
-                  onKeyDown={handleKeyPress}
-                  id={zone.skill}
-                  min="1"
-                  max={stats.freeSkillPoints}
-                  pattern="^[0-9]+"
-                />
-                <img
-                  onClick={handleClickAddSkill}
-                  src={require("../img/utilities/addStats.webp")}
-                  alt=""
-                />
+                <a
+                  className="links"
+                  onClick={() => {
+                    handleClickAddSkill(zone.skill);
+                  }}
+                  href="#"
+                >
+                  +
+                </a>
               </div>
             ))}
           </div>
