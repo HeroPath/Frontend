6158f358482b7cf49678ec96e48b41e9969d0dcd
Giancarlo <g.galvarini@alumno.um.edu.ar>
2023-02-05 21:16:56 -0300

    update

--- src/functions/utilities.js
+++ src/functions/utilities.js
@@ -54,7 +54,6 @@ export const notifySuccess = (
       draggable: true,
       progress: undefined,
       closeButton: false,
-      onClose: () => window.location.replace(redirectTo),
       className: "custom-success-toast",
     }
   );
