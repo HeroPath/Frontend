2254b84d8d23938da9942227cf73114250a23c38
Giancarlo <g.galvarini@alumno.um.edu.ar>
2023-03-16 07:23:23 -0300

    update

--- src/components/sideBar/Guild/CreateNewGuild.jsx
+++ src/components/sideBar/Guild/CreateNewGuild.jsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 import { headers, notifySuccess } from "../../../functions/utilities";
-import { post } from "../../../functions/requestsApi";
+import { post, get } from "../../../functions/requestsApi";
 
 const CreateNewGuild = () => {
-  const [values, setValues] = React.useState({
+  const [requirementesCreateGuild, setRequirementesCreateGuild] = useState({});
+  const [values, setValues] = useState({
     name: "",
     description: "",
     tag: "",
@@ -19,13 +20,16 @@ const CreateNewGuild = () => {
     if (values.name && values.description && values.tag) {
       const response = await post("/api/v1/guilds", values, headers);
       if (response.status === 200) {
-        notifySuccess(
-          "/Guild",
-          "Guild created successfully!",
-          "Name: " + values.name,
-          "Tag: " + values.tag
-        );
+        notifySuccess("/Guild", "Guild created successfully!", "Name: " + values.name, "Tag: " + values.tag);
       }
+    }
+  }
+
+  async function getRequirementesCreateGuild() {
+    const response = await get("/api/v1/guilds/requirements", headers);
+    if (response.status === 200) {
+      console.log(response.data);
+      setRequirementesCreateGuild(response.data);
     }
   }
 
@@ -41,50 +45,58 @@ const CreateNewGuild = () => {
     setValues(newValues);
   }
 
+  useEffect(() => {
+    getRequirementesCreateGuild();
+  }, []);
+
   return (
     <div>
-      <section>
-        <h1>Create a new guild</h1>
-        <h2>Requirements:</h2>
-        <h3>Level: 100</h3>
-        <h3>Gold: 500.000</h3>
-        <h3>Diamonds: 100</h3>
-        <form onSubmit={handleSubmit}>
-          <label className="form-label mt-3">Name</label>
-          <input
-            type="text"
-            id="name"
-            className="form-control mt-2"
-            name="name"
-            value={values.name}
-            onChange={handleChange}
-          />
+      {requirementesCreateGuild && (
+        <section>
+          <h1>Create a new guild</h1>
+          <h2>Requirements:</h2>
+          <h3>Level: {requirementesCreateGuild.lvl}</h3>
 
-          <label className="form-label mt-3">Tag</label>
-          <input
-            type="text"
-            id="tag"
-            className="form-control mt-2"
-            name="tag"
-            value={values.tag}
-            onChange={handleChange}
-          />
+          <h3>Gold: {requirementesCreateGuild.gold && requirementesCreateGuild.gold.toLocaleString()}</h3>
+          <h3>Diamonds: {requirementesCreateGuild.diamonds && requirementesCreateGuild.diamonds.toLocaleString()}</h3>
+          <form onSubmit={handleSubmit}>
+            <label className="form-label mt-3">Name</label>
+            <input
+              type="text"
+              id="name"
+              className="form-control mt-2"
+              name="name"
+              value={values.name}
+              onChange={handleChange}
+            />
 
-          <label className="form-label mt-3">Description</label>
-          <input
-            type="text"
-            id="description"
-            className="form-control mt-2"
-            name="description"
-            value={values.description}
-            onChange={handleChange}
-          />
+            <label className="form-label mt-3">Tag</label>
+            <input
+              type="text"
+              id="tag"
+              className="form-control mt-2"
+              name="tag"
+              value={values.tag}
+              onChange={handleChange}
+            />
 
-          <button className="pe-5 ps-5 mt-4 btn btn-success" type="submit">
-            Create
-          </button>
-        </form>
-      </section>
+            <label className="form-label mt-3">Description</label>
+            <input
+              type="text"
+              id="description"
+              className="form-control mt-2"
+              name="description"
+              value={values.description}
+              onChange={handleChange}
+            />
+
+            <button className="pe-5 ps-5 mt-4 btn btn-success" type="submit">
+              Create
+            </button>
+          </form>
+        </section>
+      )}
+
       <ToastContainer
         position="top-right"
         autoClose={2000}
