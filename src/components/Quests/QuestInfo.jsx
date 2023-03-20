20717c427c18f3395167bd2728eded23c9f651b7
Giancarlo <g.galvarini@alumno.um.edu.ar>
2023-03-19 21:05:00 -0300

    update

--- src/components/Quests/QuestInfo.jsx
+++ src/components/Quests/QuestInfo.jsx
@@ -5,13 +5,7 @@ const QuestInfo = ({ focus }) => {
         <h3>Description:</h3>
         <div className="scrollable-content">
           <h5>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae ipsam ea placeat cupiditate,
-            accusantium tempora rerum ipsa, quaerat nam adipisci labore magnam voluptatum nemo commodi asperiores iusto
-            velit? Tempore dolorem beatae atque eos omnis sequi sint inventore minus animi unde a iusto doloremque
-            maxime aut error asperiores pariatur dicta nulla alias, ipsum magni doloribus earum hic iure. Praesentium
-            sed unde vero amet porro quam eos distinctio reiciendis perspiciatis libero, odit placeat similique debitis
-            non nihil atque dolore harum corporis enim quae iste commodi temporibus. Error odio consectetur obcaecati.
-            Excepturi dolores facere mollitia distinctio vero similique fuga modi non perspiciatis eligendi!
+            {focus.quest.description}
           </h5>
         </div>
       </div>
--- src/functions/requestsApi.js
+++ src/functions/requestsApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { notify } from "./utilities";
 
-const API_URL = "http://192.168.18.8:8000";
+const API_URL = "http://localhost:8000";
 
 
 export async function get(route, headers) {
