3868c7c41ea63526684efc20ac27100ee96277fd
LucianoGalvarini <galvariniluciano@gmail.com>
2023-03-20 22:12:50 -0300

    upgrade component

--- src/components/Shop/UpgradeNPC.jsx
+++ src/components/Shop/UpgradeNPC.jsx
@@ -2,7 +2,11 @@ const UpgradeNPC = () => {
   return (
     <div className="shop--npc--section" id="shop--npc--card">
       <div className="upgrade--npc--card" id="upgradeNpc">
-        
+        <div className="upgradeDiv"></div>
+        <div className="upgradeDiv"></div>
+        <div className="upgradeDiv"></div>
+        <div className="upgradeDiv"></div>
+        <div className="upgradeDiv"></div>
       </div>
     </div>
   );
